5aa7d71d7983b4fef1ed3d75937103d74ee65aaf
𝕯𝖎𝖑𝖓𝖚𝖗𝖆 <adilnura2197@gmail.com>
2026-05-06 14:47:17 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+class Odam:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        self.money = 0
+        
+
+o1 = Odam("dwxsd", "ewdwe")
+print(o1.a)
+print(o1.b)
+print(o1.money)
